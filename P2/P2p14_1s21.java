f3171fd66daf711737aa1c36581993bb0ab8a43e
Jacobo Flores Perez <144180440+JacoboFlores@users.noreply.github.com>
2023-10-27 18:36:30 -0600

    Add files via upload

--- P2/P2p14_1s21.java
+++ P2/P2p14_1s21.java
@@ -4,7 +4,7 @@ public class P2p14_1s21{
 	public static void main(String[] args) {
 		Scanner teclado = new  Scanner(System.in);
 		int[][]Ma,Mb,Mc;
-		int fa,ca,fb,cb,f,c,r,h;
+		int fa,ca,fb,cb,f,c,r=0,h;
 		System.out.println("Ingresa el numero de filas para la Matriz A");
 		fa = teclado.nextInt();
 		System.out.println("Ingresa el numero de colunas para la Matriz A");
@@ -14,9 +14,9 @@ public class P2p14_1s21{
 		System.out.println("Ingresa el numero de colunas para la Matriz B");
 		cb = teclado.nextInt();
 		if (ca == fb){
-			Ma = int[fa][fa];
-			Mb = int[fb][cb];
-			Mc = int[fa][cb];
+			Ma = new int[fa][fa];
+			Mb = new int[fb][cb];
+			Mc = new int[fa][cb];
 			for(f=0;f<fa;f++){
 				for (c=0;c<ca ;c++ ){
 				System.out.println("Ingresa el valor para Ma["+f+"]["+c+"]: ");
@@ -34,9 +34,10 @@ public class P2p14_1s21{
         for (f=0;f<fb;f++){
         	for (c=0;c<cb;c++){
         		for (h=0;h<cb;h++){
-        			r=Ma[f][h]*Mb[c][h]
+        			r=Ma[f][h]*Mb[c][h];
         			
         		}
+        		Mc[f][c]=r;
         	}
         	
         }
